ec958ee4bc3bad1926354adcba00fdcdf5bc3217
Furkan Ertekin <furkanertekin0@gmail.com>
2023-02-02 17:35:08 +0300

    kullanici liste

---
+++ KullaniciListe/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace MessageBoxUygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIsim = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyisim = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmailAdres = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTelefonNo = new System.Windows.Forms.MaskedTextBox();
+            this.btnYeniKayit = new System.Windows.Forms.Button();
+            this.lstMusteriler = new System.Windows.Forms.ListBox();
+            this.bildirimCubugu = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnYeniKayit);
+            this.groupBox1.Controls.Add(this.txtTelefonNo);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtEmailAdres);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSoyisim);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtIsim);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 188);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Yeni Müşteri Ekleme Formu";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstMusteriler);
+            this.groupBox2.Location = new System.Drawing.Point(329, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(459, 188);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Müşteri kayıt Listesi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(107, 30);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(134, 20);
+            this.txtIsim.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyisim";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(107, 56);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(134, 20);
+            this.txtSoyisim.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email Adresi";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtEmailAdres
+            // 
+            this.txtEmailAdres.Location = new System.Drawing.Point(107, 82);
+            this.txtEmailAdres.Name = "txtEmailAdres";
+            this.txtEmailAdres.Size = new System.Drawing.Size(134, 20);
+            this.txtEmailAdres.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Telefon Numarası";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtTelefonNo
+            // 
+            this.txtTelefonNo.Location = new System.Drawing.Point(107, 108);
+            this.txtTelefonNo.Name = "txtTelefonNo";
+            this.txtTelefonNo.Size = new System.Drawing.Size(134, 20);
+            this.txtTelefonNo.TabIndex = 3;
+            // 
+            // btnYeniKayit
+            // 
+            this.btnYeniKayit.Location = new System.Drawing.Point(107, 135);
+            this.btnYeniKayit.Name = "btnYeniKayit";
+            this.btnYeniKayit.Size = new System.Drawing.Size(134, 27);
+            this.btnYeniKayit.TabIndex = 4;
+            this.btnYeniKayit.Text = "Yeni Müşteri Ekle";
+            this.btnYeniKayit.UseVisualStyleBackColor = true;
+            this.btnYeniKayit.Click += new System.EventHandler(this.btnYeniKayit_Click);
+            // 
+            // lstMusteriler
+            // 
+            this.lstMusteriler.FormattingEnabled = true;
+            this.lstMusteriler.Location = new System.Drawing.Point(15, 19);
+            this.lstMusteriler.Name = "lstMusteriler";
+            this.lstMusteriler.Size = new System.Drawing.Size(295, 160);
+            this.lstMusteriler.TabIndex = 0;
+            // 
+            // bildirimCubugu
+            // 
+            this.bildirimCubugu.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 247);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Message Box Uygulama";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MaskedTextBox txtIsim;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnYeniKayit;
+        private System.Windows.Forms.MaskedTextBox txtTelefonNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox txtEmailAdres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox txtSoyisim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstMusteriler;
+        private System.Windows.Forms.NotifyIcon bildirimCubugu;
+    }
+}
+
---
+++ KullaniciListe/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYeniKayit_Click(object sender, EventArgs e)
+        {
+            int islemSonuc = yeniMusteriEkle(new Musteri()
+            {
+                id = Guid.NewGuid(),
+                isim = txtIsim.Text,
+                soyisim = txtSoyisim.Text,
+                emailAdres = txtEmailAdres.Text,
+                telefonNumarasi = txtTelefonNo.Text,
+            });
+
+            if (islemSonuc > 0)
+            {
+                DialogResult res = MessageBox.Show("Müşteri ekleme işlemi başarılı , yeni müşteri kaydı eklemek ister misiniz?", "Bilgilendirme", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (res == DialogResult.Yes)
+                {
+                    bildirimCubugu = new NotifyIcon();
+                    bildirimCubugu.BalloonTipText = "Toplam müşteri kayıt adedi" + SanalDataBase.musteriler.Count.ToString();
+                    bildirimCubugu.BalloonTipTitle = "Müşteri adet bilgisi ";
+                    bildirimCubugu.Visible = true;
+                    bildirimCubugu.Icon = SystemIcons.Information;
+                    bildirimCubugu.ShowBalloonTip(2000);
+                }
+                else if (res == DialogResult.No)
+                {
+
+                }
+
+                ekranTemizle();
+                ekranListele();
+            }
+            else
+            {
+                MessageBox.Show("Hata : Kayıt yenileme işlemi yapılamadı.");
+            }
+        }
+
+        private void ekranListele()
+        {
+            lstMusteriler.DataSource = SanalDataBase.musteriler;
+        }
+
+        private void ekranTemizle()
+        {
+            txtIsim.Text = string.Empty;
+            txtSoyisim.Text = string.Empty;
+            txtEmailAdres.Text = string.Empty;
+            txtTelefonNo.Text = string.Empty;
+        }
+
+        private int yeniMusteriEkle(Musteri data)
+        {
+            SanalDataBase.musteriler.Add(data);
+            return 1;
+        }
+    }
+}
