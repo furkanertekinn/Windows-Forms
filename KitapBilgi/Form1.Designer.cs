5599fdf04b1e6d9ae6153e9284384c5844ce7670
Furkan Ertekin <furkanertekin0@gmail.com>
2023-02-13 16:05:13 +0300

    kitap bilgi

--- KitapBilgi/Form1.Designer.cs
+++ KitapBilgi/Form1.Designer.cs
@@ -31,16 +31,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.cmbUrunListe = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pctUrunResim = new System.Windows.Forms.PictureBox();
-            this.txtUrunAdi = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtKategori = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.txtUrunAciklama = new System.Windows.Forms.RichTextBox();
+            this.txtYazar = new System.Windows.Forms.TextBox();
             this.txtStokAdet = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.txtYazar = new System.Windows.Forms.TextBox();
-            this.txtUrunAciklama = new System.Windows.Forms.RichTextBox();
+            this.txtKategori = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pctUrunResim = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctUrunResim)).BeginInit();
             this.SuspendLayout();
@@ -82,58 +82,20 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Seçilen Ürün Detay Bilgisi";
             // 
-            // label2
+            // txtUrunAciklama
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(108, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Ürün Adı";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.txtUrunAciklama.Location = new System.Drawing.Point(6, 135);
+            this.txtUrunAciklama.Name = "txtUrunAciklama";
+            this.txtUrunAciklama.Size = new System.Drawing.Size(332, 101);
+            this.txtUrunAciklama.TabIndex = 3;
+            this.txtUrunAciklama.Text = "";
             // 
-            // pctUrunResim
+            // txtYazar
             // 
-            this.pctUrunResim.Location = new System.Drawing.Point(6, 20);
-            this.pctUrunResim.Name = "pctUrunResim";
-            this.pctUrunResim.Size = new System.Drawing.Size(84, 98);
-            this.pctUrunResim.TabIndex = 1;
-            this.pctUrunResim.TabStop = false;
-            this.pctUrunResim.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // txtUrunAdi
-            // 
-            this.txtUrunAdi.Location = new System.Drawing.Point(162, 20);
-            this.txtUrunAdi.Name = "txtUrunAdi";
-            this.txtUrunAdi.Size = new System.Drawing.Size(176, 20);
-            this.txtUrunAdi.TabIndex = 2;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(108, 49);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 13);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Kategori";
-            this.label3.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // txtKategori
-            // 
-            this.txtKategori.Location = new System.Drawing.Point(162, 46);
-            this.txtKategori.Name = "txtKategori";
-            this.txtKategori.Size = new System.Drawing.Size(176, 20);
-            this.txtKategori.TabIndex = 2;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(108, 75);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Stok Adet";
-            this.label4.Click += new System.EventHandler(this.label2_Click);
+            this.txtYazar.Location = new System.Drawing.Point(162, 98);
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(176, 20);
+            this.txtYazar.TabIndex = 2;
             // 
             // txtStokAdet
             // 
@@ -152,20 +114,58 @@
             this.label5.Text = "Yazar";
             this.label5.Click += new System.EventHandler(this.label2_Click);
             // 
-            // txtYazar
+            // txtKategori
             // 
-            this.txtYazar.Location = new System.Drawing.Point(162, 98);
-            this.txtYazar.Name = "txtYazar";
-            this.txtYazar.Size = new System.Drawing.Size(176, 20);
-            this.txtYazar.TabIndex = 2;
+            this.txtKategori.Location = new System.Drawing.Point(162, 46);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.Size = new System.Drawing.Size(176, 20);
+            this.txtKategori.TabIndex = 2;
             // 
-            // txtUrunAciklama
+            // label4
             // 
-            this.txtUrunAciklama.Location = new System.Drawing.Point(6, 135);
-            this.txtUrunAciklama.Name = "txtUrunAciklama";
-            this.txtUrunAciklama.Size = new System.Drawing.Size(332, 101);
-            this.txtUrunAciklama.TabIndex = 3;
-            this.txtUrunAciklama.Text = "";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Stok Adet";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(162, 20);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(176, 20);
+            this.txtUrunAdi.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kategori";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pctUrunResim
+            // 
+            this.pctUrunResim.Location = new System.Drawing.Point(6, 20);
+            this.pctUrunResim.Name = "pctUrunResim";
+            this.pctUrunResim.Size = new System.Drawing.Size(84, 98);
+            this.pctUrunResim.TabIndex = 1;
+            this.pctUrunResim.TabStop = false;
+            this.pctUrunResim.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ürün Adı";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
@@ -178,7 +178,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ComboBox Inceleme";
+            this.Text = "Kitap Bölümü";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
